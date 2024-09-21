38a1d3a380fc2d149f3280e5315343cc1dbe7cf3
Abani Senapati <v-senapatia@microsoft.com>
2024-09-21 02:06:28 -0700

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,15 +42,51 @@ class ModelTrainer:
             models={
                 "Linear Regressor":LinearRegression(),
                 "DecisionTree Regressor":DecisionTreeRegressor(),
-                "K-Neighbors Regressor":KNeighborsRegressor(),
                 "Randomforest Regressor":RandomForestRegressor(),
                 "Adaboost Regressor":AdaBoostRegressor(),
                 "GradientBoosting Regressor":GradientBoostingRegressor(),
                 "XgBoosting Regressor":XGBRegressor(),
                 "CatBoosting Regressor":CatBoostRegressor()
             }
+            params={
+                "DecisionTree Regressor": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Randomforest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "GradientBoosting Regressor":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regressor":{},
+                "XgBoosting Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "Adaboost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
 
-            model_report:dict=evaluate_model(x_train,y_train,x_test,y_test,models)
+            model_report:dict=evaluate_model(x_train,y_train,x_test,y_test,models,params=params)
             # print("Model trained")
 
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 
 from src.exception import custom_exception
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -18,19 +19,26 @@ def save_object(file_path,obj):
         raise custom_exception(e,sys)
     
 
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,params):
     try:
         report={}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
 
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
 
             train_model_score = r2_score(y_train,y_train_pred)
-
+            print(f"train score:{train_model_score}")
             test_model_score = r2_score(y_test,y_test_pred)
+            print(f"test score:{test_model_score}")
 
             report[list(models.keys())[i]] = test_model_score
         # print(report)
